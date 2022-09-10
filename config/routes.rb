dcd377980c4d4f70fae4b7fda2f9a19f751fdbb6
Ian Iversen <ianiversen1@gmail.com>
2022-09-10 17:20:00 -0400

    commit tree 0a0d47f7758378ee1fcc6ec3074c75172faaabbc

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  post "/signup", to: "user#create"
+  get "/show", to: "user#show"
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
